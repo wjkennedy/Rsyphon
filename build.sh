d6970427d5beb1f9a425556864c12f5ba0aaad39
William Kennedy <william@a9group.net>
2017-10-20 22:42:48 -0700

    build.sh - disable docs for now.

--- build.sh
+++ build.sh
@@ -21,6 +21,6 @@ cat VERSION
 sudo apt-get build-dep rsyphon
 sudo apt-get install wget libssl-dev
 autoconf ./config.inc.in
-./configure
-sudo make 
-sudo make deb
+./configure --disable-docs
+sudo make
+sudo make deb 
